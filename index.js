e25db36763662bc040ac43963f4379a6aed8505f
hnnav <57915266+hnnav@users.noreply.github.com>
2022-10-03 14:18:47 +0300

    getting data from form

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
+function showNav() {
+  const navDropDown = document.querySelector(".nav-dropdown");
+  document.getElementById('hide-image').style.visibility='hidden';
+  let displayStatus = navDropDown.style.display
+  if (displayStatus === "none" || displayStatus === "") {
+    navDropDown.style.display = "block";
+    document.getElementById('hide-image').style.visibility='hidden';
+  } else {
+    navDropDown.style.display = "none";
+    document.getElementById('hide-image').style.visibility='visible';
+  }
+}
 
- 
+document.querySelector('.link-input-section').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    console.log(formData.get('long-link'));
+});
 
-
-      function showNav() {
-        const navDropDown = document.querySelector(".nav-dropdown");
-        document.getElementById('hide-image').style.visibility='hidden';
-        let displayStatus = navDropDown.style.display
-
-        if (displayStatus === "none" || displayStatus === "") {
-          navDropDown.style.display = "block";
-          document.getElementById('hide-image').style.visibility='hidden';
-        } else {
-          navDropDown.style.display = "none";
-          document.getElementById('hide-image').style.visibility='visible';
-        }
-      }
+// Next: send long link to API and retrieve short link
