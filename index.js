a530738fb66b97e93c77470dca59ab07b2b27417
hnnav <57915266+hnnav@users.noreply.github.com>
2022-10-05 10:25:52 +0300

    API

--- index.js
+++ index.js
@@ -10,3 +10,36 @@ function showNav() {
     document.getElementById('hide-image').style.visibility='visible';
   }
 }
+
+const linkSearch = document.getElementById('link-search');
+
+linkSearch.addEventListener('submit', function (e) { 
+    e.preventDefault();
+    
+    const formData = new FormData(this);
+    longLink = formData.get('long-link')
+    const linksContainer = document.querySelector('.links')
+
+    fetch ('https://api.shrtco.de/v2/shorten?url=' + longLink) 
+    .then(function (response) { 
+        return response.json(); 
+    })  
+    
+    .then(function(text) { 
+        const newDiv = document.createElement("div");
+        const longLinkDisplay = document.createElement("p");
+        const shortLinkDisplay = document.createElement("p");
+        const copyButton = document.createElement("button");
+
+        longLinkDisplay.innerText = longLink;
+        shortLinkDisplay.innerText = text.result.short_link;
+        copyButton.innerText = 'copy';
+
+        newDiv.append(longLinkDisplay, shortLinkDisplay, copyButton);
+        linksContainer.append(newDiv);
+    }) 
+
+    .catch(function (error) { 
+        console.error(error); 
+    })
+})
